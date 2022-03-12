3eb4a4357fcad2cc579fb0059e9425c426a29805
Jay Chopra <81903319+jaychopra04@users.noreply.github.com>
2022-03-12 19:40:53 +0530

    updated title with page readibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,3 +110,6 @@ particlesJS("particles-js", {
   "retina_detect": true
 });
 
+
+
+var hidden,visibilityChange;let a;function handleVisibilityChange(){document[hidden]?a=setInterval(()=>{"Aww..."==document.title?document.title="Please Come Back":document.title="Aww..."},1500):(document.title="Jay Chopra",clearInterval(a))}void 0!==document.hidden?(hidden="hidden",visibilityChange="visibilitychange"):void 0!==document.msHidden?(hidden="msHidden",visibilityChange="msvisibilitychange"):void 0!==document.webkitHidden&&(hidden="webkitHidden",visibilityChange="webkitvisibilitychange"),void 0===document.addEventListener||void 0===hidden?console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API."):document.addEventListener(visibilityChange,handleVisibilityChange,!1)
