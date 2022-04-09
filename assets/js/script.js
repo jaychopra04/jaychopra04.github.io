85380ebbb1c55cbffdcfe0e0c8d188785e041ad3
Jay Chopra <81903319+jaychopra04@users.noreply.github.com>
2022-04-09 18:29:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var hidden,visibilityChange;let a;function handleVisibilityChange(){document[hidden]?a=setInterval(()=>{"Aww..."==document.title?document.title="Please Come Back":document.title="Aww..."},1500):(document.title="Jay Chopra",clearInterval(a))}void 0!==document.hidden?(hidden="hidden",visibilityChange="visibilitychange"):void 0!==document.msHidden?(hidden="msHidden",visibilityChange="msvisibilitychange"):void 0!==document.webkitHidden&&(hidden="webkitHidden",visibilityChange="webkitvisibilitychange"),void 0===document.addEventListener||void 0===hidden?console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API."):document.addEventListener(visibilityChange,handleVisibilityChange,!1)
+
 /* ---- particles.js config ---- */
 
 particlesJS("particles-js", {
@@ -112,4 +114,3 @@ particlesJS("particles-js", {
 
 
 
-var hidden,visibilityChange;let a;function handleVisibilityChange(){document[hidden]?a=setInterval(()=>{"Aww..."==document.title?document.title="Please Come Back":document.title="Aww..."},1500):(document.title="Jay Chopra",clearInterval(a))}void 0!==document.hidden?(hidden="hidden",visibilityChange="visibilitychange"):void 0!==document.msHidden?(hidden="msHidden",visibilityChange="msvisibilitychange"):void 0!==document.webkitHidden&&(hidden="webkitHidden",visibilityChange="webkitvisibilitychange"),void 0===document.addEventListener||void 0===hidden?console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API."):document.addEventListener(visibilityChange,handleVisibilityChange,!1)
